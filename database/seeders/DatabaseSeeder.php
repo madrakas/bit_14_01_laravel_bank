be3e77fac1bbb1f60a192e099381d1feaf82f1a0
Arvydas <arvydas.simbelis@gmail.com>
2024-02-07 21:29:52 +0200

    Initial commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,20 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+
+        DB::table('users')->insert([
+            'first_name' => 'Vilkas',
+            'last_name' => 'Pilkas',
+            'email' => 'vilkas.pilkas@zaliasmiskas.lt',
+            'password' => Hash::make('123'),
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Lapė',
+            'last_name' => 'Snapė',
+            'email' => 'lape.snape@zaliasmiskas.lt',
+            'password' => Hash::make('123'),
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Auth::routes();
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
