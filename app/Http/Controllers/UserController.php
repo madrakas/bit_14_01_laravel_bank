b7c1840ff50366346ccc7e0c630ca0b25d4490f7
Arvydas <arvydas.simbelis@gmail.com>
2024-02-08 20:12:23 +0200

    Show user details

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -66,4 +66,8 @@ class UserController extends Controller
         
         return redirect()->route('home')->with('success', "Password Changed Successfully");
     }
+
+    public function show(){
+        return view('user.show');
+    }
 }
