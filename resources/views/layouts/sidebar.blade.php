e8a19bc02217804c8d0b70bc718b8c3b169fee0e
Arvydas <arvydas.simbelis@gmail.com>
2024-02-10 18:28:04 +0200

    User menu sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,37 +2,32 @@
             <div class="row flex-nowrap">
                 <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0">
                     <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="width: 280px;">
+                        <span class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                        <span class="fs-4">User menu</span>
+                        </span>
+                        <hr/>
                         <ul class="nav nav-pills flex-column mb-auto">
                             <li class="nav-item">
-
                             <li class="">  
-
-
-
                                 <a href="{{ route('user-show-accounts') }}" class="nav-link {{ (Route::currentRouteName() === 'user-show-accounts') ? 'active' : 'link-body-emphasis' }}" {{ (Route::currentRouteName() === 'user-show-accounts') ? 'aria-current="page"' : '' }}>
-                                    <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
                                     Accounts
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="nav-link link-body-emphasis">
-                                    <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
                                     New transaction
                                 </a>
                             </li>
                             <li>
                                 <a href="#" class="nav-link link-body-emphasis">
-                                    <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
                                     Transactions history
                                 </a>
                             </li>
                             <li>
                                 <a href="{{ route('user-show')}}" class="nav-link {{ (Route::currentRouteName() === 'user-show') ? 'active' : 'link-body-emphasis' }}" {{ (Route::currentRouteName() === 'user-show') ? 'aria-current="page"' : '' }}>
-                                    <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
                                     User profile
                                 </a>
                             </li>
-                            
                         </ul>
                     </div>
                 </div>
